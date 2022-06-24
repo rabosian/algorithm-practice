be1a8c96790b149365d5eac1a06829d9b8643ac1
rabosian <msann1356@gmail.com>
2022-06-24 13:22:29 -0400

    2022-06-24

---
+++ leetcode/Top-Interview-Questions/LinkedList/reverseLinkedList.py
@@ -0,0 +1,24 @@
+# (i) three pointer method (iterative)
+def reverseList_iterative(head):
+    if head == None or head.next == None:
+        return head
+    prev = None # the last element's next == Null
+    cur, next = head, head
+    while cur:
+        next = cur.next
+        cur.next = prev
+        prev = cur
+        cur = next
+    head = prev
+    return head
+
+
+# (ii)recursive
+def reverseList_recursive(head):
+    if head == None or head.next == None:
+        return head
+    
+    rest = self.reverseList(head.next) # self for leetcode submission
+    head.next.next = head
+    head.next = None
+    return rest
