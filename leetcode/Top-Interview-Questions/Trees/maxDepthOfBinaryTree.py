686fadcbef31dfa8e1e30ace8e2f070e49a31461
rabosian <msann1356@gmail.com>
2022-06-30 12:42:29 -0400

    2022-06-30

---
+++ leetcode/Top-Interview-Questions/Trees/maxDepthOfBinaryTree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def maxDepth(root):
+    if root is None:
+        return 0
+    return max(maxDepth(root.left), maxDepth(root.right))
+
+
+print(max(None, 1))
+root = TreeNode(3) 
+root.left = TreeNode(9) 
+root.right = TreeNode(20) 
+root.right.left = TreeNode(15) 
+root.right.right = TreeNode(7) 
+
+print(maxDepth(root))
