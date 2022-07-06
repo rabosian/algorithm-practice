3ac9396cebb810762bee7bde1412b152e96e5453
rabosian <msann1356@gmail.com>
2022-07-06 15:55:53 -0400

    2022-07-06

--- leetcode/Top-Interview-Questions/Trees/maxDepthOfBinaryTree.py
+++ leetcode/Top-Interview-Questions/Trees/maxDepthOfBinaryTree.py
@@ -1,21 +1,53 @@
 # Definition for a binary tree node.
+from collections import deque
+
+
 class TreeNode(object):
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
+# DFS recursion: O(N)
 def maxDepth(root):
     if root is None:
         return 0
-    return max(maxDepth(root.left), maxDepth(root.right)) + 1
+    return 1 + max(maxDepth(root.left), maxDepth(root.right))
 
 
-print(max(None, 1))
+# DFS iteration:
+def maxDepth_iterative(root):
+    stack = [[root, 1]]
+    depth = 0
+    while stack:
+        node, level = stack.pop()
+        if node:
+            depth = max(depth, level)
+            stack.append([node.left, level+1])
+            stack.append([node.right, level+1])
+    return depth
+
+
+# BFS recursion: O(N)
+def maxDepth_bfs(root):
+    if not root:
+        return 0
+    queue, depth = deque([root]), 0
+    while queue:
+        for i in range(len(queue)):
+            cur = queue.popleft()
+            if cur.left:
+                queue.append(cur.left)
+            if cur.right:
+                queue.append(cur.right)
+        depth += 1
+    return depth
+
+
 root = TreeNode(3) 
 root.left = TreeNode(9) 
 root.right = TreeNode(20) 
 root.right.left = TreeNode(15) 
 root.right.right = TreeNode(7) 
 
-print(maxDepth(root))
+print(maxDepth_iterative(root))
