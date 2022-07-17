ad38327741c65696324b9be2fd337f5ec8b128d8
rabosian <msann1356@gmail.com>
2022-07-17 18:44:36 -0400

    2022-07-17

---
+++ hackerrank/1wPreparationKit/day3/towerBreakers.py
@@ -0,0 +1,11 @@
+def towerBreakers(n, m):
+    if m == 1:
+        return 2
+    else:
+        if n % 2 == 1:
+            return 1
+        else:
+            return 2
+
+
+print(towerBreakers(6,6))
---
+++ hackerrank/1wPreparationKit/day3/zigzagSeequence.py
@@ -0,0 +1,22 @@
+def findZigZagSequence(a, n):
+    a.sort() 
+    mid = int((n)/2) 
+    a[mid], a[n-1] = a[n-1], a[mid] 
+
+    st = mid + 1
+    ed = n - 2
+    while(st <= ed):
+        a[st], a[ed] = a[ed], a[st]
+        st = st + 1
+        ed = ed - 1
+
+    for i in range (n):
+        if i == n-1:
+            print(a[i])
+        else:
+            print(a[i], end = ' ')
+    return
+
+
+a = [1,5,2,4,3,6,7]
+findZigZagSequence(a, len(a))
