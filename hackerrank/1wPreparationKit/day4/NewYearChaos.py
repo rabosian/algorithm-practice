9bec62f5ebd32ce154e22c7462484711bd6be03a
rabosian <msann1356@gmail.com>
2022-08-01 18:08:41 -0400

    2022-08-01(2)

--- hackerrank/1wPreparationKit/day4/NewYearChaos.py
+++ hackerrank/1wPreparationKit/day4/NewYearChaos.py
@@ -1,8 +1,22 @@
 def minimumBribes(q):
+    bribes = 0
+    swaps = [1,2,3]
+    for i in range(len(q)):
+        if q[i] == swaps[0]:
+            swaps.append(swaps[2]+1)
+            swaps.pop(0)
+        elif q[i] == swaps[1]:
+            swaps.append(swaps[2]+1)
+            swaps.pop(1)
+            bribes += 1
+        elif q[i] == swaps[2]:
+            swaps[2] += 1
+            bribes += 2
+        else:
+            print("Too chaotic")
+            return
+    print(bribes)
 
 
-
-
-
-q = [2,1,5,3,4]
-print(minimumBribes(q))
+q = [2,5,1,3,4]
+minimumBribes(q)
