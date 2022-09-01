85438201f4a65503ac1ebcfd68107d6a3a28789b
rabosian <msann1356@gmail.com>
2022-08-31 20:27:28 -0400

    2022-08-31

--- leetcode/Others/48.RotateImage.py
+++ leetcode/Others/48.RotateImage.py
@@ -1,12 +1,6 @@
 def rotate(matrix):
-    n = len(matrix)
-    for i in range(n):
-        for j in range(i,n):
-            matrix[i][j], matrix[j][i] = matrix[j][i],matrix[i][j]
-
-        matrix[i].reverse()
-
-
-
-matrix = [[1,2,3],[4,5,6],[7,8,9]]
-print(rotate(matrix))
+    rows = [row for row in matrix]
+    cols = list(zip(*matrix))
+    for i in range(len(matrix)):
+        cols[i] = list(reversed(cols[i]))
+        matrix[i] = cols[i] 
