794e37dcb57ef8a1fc08c61c8ea4656c251349e4
rabosian <msann1356@gmail.com>
2022-10-01 23:42:32 -0400

    48 add solution

--- leetcode/Others/48.RotateImage.py
+++ leetcode/Others/48.RotateImage.py
@@ -1,6 +1,19 @@
+# def rotate(matrix):
+#     rows = [row for row in matrix]
+#     cols = list(zip(*matrix))
+#     for i in range(len(matrix)):
+#         cols[i] = list(reversed(cols[i]))
+#         matrix[i] = cols[i] 
+
 def rotate(matrix):
-    rows = [row for row in matrix]
-    cols = list(zip(*matrix))
+    matrix.reverse()
     for i in range(len(matrix)):
-        cols[i] = list(reversed(cols[i]))
-        matrix[i] = cols[i] 
+        for j in range(i):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+matrix = [[5,1,9,11],
+          [2,4,8,10],
+          [13,3,6,7],
+          [15,14,12,16]]
+rotate(matrix)
+print(matrix)
